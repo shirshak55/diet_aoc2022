09930b7b6d1a061f2cb1764fca1ab527eb9918b2
shirshak55 <shirshak55@gmail.com>
2022-12-01 23:21:12 -0600

    day_1

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Diet Advent of Code
+
+Originally started by museun
+
+If you can find lean solution feel free to submit PR.
+
+Rules:
+
+1. No semicolon
+2. No if
+3. Ask [https://www.twitch.tv/museun](museun) :D
+
+To run
+
+```
+cargo run --bin day_1
+```
+
+```
+cargo run --bin day_2
+```
---
+++ src/dec_1_part_a.rs
@@ -0,0 +1,36 @@
+fn main() {
+    std::fs::read_to_string("./inputs/dec_1.txt")
+        .ok()
+        .map(|v| {
+            v.lines()
+                .map(|w| (!w.is_empty()).then(|| w.parse::<isize>().unwrap()))
+                .collect::<Vec<_>>()
+        })
+        .unwrap()
+        .into_iter()
+        .fold(vec![vec![]], |mut accum, value| {
+            std::iter::once(
+                value
+                    .is_none()
+                    .then(|| accum.push(vec![]))
+                    .unwrap_or_else(|| accum.last_mut().unwrap().push(value.unwrap())),
+            )
+            .last()
+            .map(|_| accum)
+            .unwrap()
+        })
+        .into_iter()
+        .map(|w: Vec<isize>| w.into_iter().sum())
+        .map(|v: isize| Some(v))
+        .collect::<Option<Vec<isize>>>()
+        .map(|mut v| {
+            std::iter::once(v.sort_by(|a, b| a.cmp(b)))
+                .last()
+                .map(|_| v)
+                .unwrap()
+        })
+        .unwrap()
+        .last()
+        .map(|v| println!("The answer is {v}"))
+        .unwrap()
+}
